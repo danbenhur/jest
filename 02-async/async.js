92fdf5039e9c40cc5c2c78a6b3a048d0ab31dc49
Dan <danbenhur@protonmail.com>
2022-07-27 17:17:59 +0300

    finished crash course number 1

--- 02-async/async.js
+++ 02-async/async.js
@@ -4,7 +4,6 @@ const fetchData = async (id) => {
   const results = await axios.get(
     `https://jsonplaceholder.typicode.com/todos/${id}`
   );
-  console.log(results.data);
   return results.data;
 };
 
